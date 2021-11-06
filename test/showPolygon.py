9ae7c9c87ec5891200c9495282f0573a1e7dc1ef
Aljoscha Schmidt <aljoscha97@web.de>
2021-11-06 11:28:33 +0100

    added first tests for iterator functions

---
+++ test/showPolygon.py
@@ -0,0 +1,123 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# corners should be in correct order (random direction)
+INPUT_STRING = """hector_math::Polygon<Scalar> result(2, 8);
+        result.col(0) << 0, 5;
+        result.col(1) << -3.6,3.5;
+        result.col(2) << -5,0;
+        result.col(3) << -3.6,-3.5;
+        result.col(4) << 0, -5;
+        result.col(5) << 3.6, -3.5;
+        result.col(6) << 5,0;
+        result.col(7) << 3.6,3.5;"""
+
+
+def get_corners(string):
+    corners = []
+    start = 0
+    temp = 0
+    end = 0
+    while True:
+        start = string.find("<<", end)
+        end = string.find(",", start)
+        if start == -1 or end == -1:
+            return corners
+        print(f"Loop {temp}: potential first number #{string[start + 3:end].strip()}# with start {start} and end{end}")
+        x = float(string[start + 3:end])
+        start = end + 1
+        end = string.find(";", start)
+        if start == -1 or end == -1:
+            return corners
+        print(f"Loop {temp}: potential second number #{string[start:end].strip()}# with start {start} and end{end}")
+        y = float(string[start:end])
+        corners.append((x, y))
+        temp += 1
+        if end + 3 > len(string):
+            return corners
+
+
+def draw_polygon(corners):
+    x = np.zeros(len(corners) + 1)
+    y = np.zeros(len(corners) + 1)
+    for i in range(len(corners)):
+        x[i] = corners[i][0]
+        y[i] = corners[i][1]
+    x[-1] = x[0]
+    y[-1] = y[0]
+    plt.plot(x, y, linewidth=4)
+    points_x = []
+    points_y = []
+    for i in range(int(np.floor(np.min(x))), int(np.ceil(np.max(x))) + 1):
+        for j in range(int(np.floor(np.min(y))), int(np.ceil(np.max(y))) + 1):
+            points_x.append(i)
+            points_y.append(j)
+            plt.plot([i, i + 0.5], [j, j + 0.5], c="blue")
+    # plt.scatter(points_x,points_y,c="red")
+    real_points = get_real_points(corners)
+    plt.scatter([x[0] for x in real_points], [x[1] for x in real_points], c="red")
+    plt.show()
+    get_real_positions_lazy(real_points)
+
+
+def get_real_points(corners):
+    lines = []
+    real_points = []
+    for i in range(len(corners) - 1):
+        lines.append((corners[i][0], corners[i][1], corners[i + 1][0], corners[i + 1][1]))
+    lines.append((corners[-1][0], corners[-1][1], corners[0][0], corners[0][1]))
+    # sort lines by min y
+    lines.sort(key=lambda x: min(x[1], x[3]))
+    miny = min([min(x[1], x[3]) for x in lines])
+    maxy = max([max(x[1], x[3]) for x in lines])
+    y = np.floor(miny) - 0.5
+    while y < maxy + 1.5:
+        relevant_lines = [x for x in lines if min(x[1], x[3]) <= y < max(x[1], x[3])]
+        for x in relevant_lines:
+            a = min(x[1], x[3]) <= y <= max(x[1], x[3])
+            b = x[1] == y
+            c = x[3] == y
+        if len(relevant_lines) == 0:
+            y += 1
+            continue
+        minx = min([min(x[0], x[2]) for x in relevant_lines])
+        maxx = max([max(x[0], x[2]) for x in relevant_lines])
+        crossings = []
+        for line in relevant_lines:
+            temp = (y - line[3]) / (line[1] - line[3])
+            if 0 <= temp <= 1:
+                crossings.append(line[2] + temp * (line[0] - line[2]))
+        crossings.sort()
+        x = np.floor(minx) - 0.5
+        crossing_index = 0
+        while x < maxx and crossing_index < len(crossings):
+            # count crossing until point
+            while crossings[crossing_index] < x:
+                crossing_index += 1
+                if crossing_index == len(crossings):
+                    break
+            # crossing_index = max(0,crossing_index-1)
+            if crossing_index % 2 == 1:
+                real_points.append((x - 0.5, y - 0.5))
+            x += 1
+        y += 1
+    print(real_points)
+    return real_points
+
+
+def get_real_positions_lazy(real_points):
+    s = "{"
+    y_old = real_points[0][1]
+    for point in real_points:
+        if point[1] > y_old:
+            s = s + "\n"
+            y_old = point[1]
+        s = s + "{" + str(point[0]) + "," + str(point[1]) + "},"
+    s = s[0:-1] + "};"
+    print(s)
+
+
+if __name__ == "__main__":
+    corners = get_corners(INPUT_STRING)
+    draw_polygon(corners)
+    # get_real_points(corners)
---
+++ test/test_hector_iterators.cpp
@@ -0,0 +1,237 @@
+
+#include <hector_math/iterators/circle_iterator.h>
+#include <hector_math/iterators/polygon_iterator.h>
+
+#include <ros/ros.h>
+#include <gtest/gtest.h>
+
+using namespace hector_math;
+
+template<typename Scalar>
+hector_math::Polygon<Scalar> createPolygon(int which)
+{
+    if (which == 0) {
+        hector_math::Polygon<Scalar> result(2, 15);
+        result.col(0) << 0, 4.9;
+        result.col(1) << 3.5, 4.9;
+        result.col(2) << 4.3333, 3.3333;
+        result.col(3) << 4, 2.3333;
+        result.col(4) << 0, 2;
+        result.col(5) << 0, 0.95;
+        result.col(6) << 4, 0;
+        result.col(7) << 0, 0;
+        result.col(8) << -0.8, -1.0;
+        result.col(9) << -0, -2.0;
+        result.col(10) << -4.01, -0.99;
+        result.col(11) << -4, 1;
+        result.col(12) << -3, 4;
+        result.col(13) << -2, 4;
+        result.col(14) << -1.0, 2.5;
+        return result;
+    }else if (which == 1){
+        // Z structure
+        hector_math::Polygon<Scalar> result(2, 8);
+        result.col(0) << 1.66666, 4;
+        result.col(1) << -5,4;
+        result.col(2) << -5,3;
+        result.col(3) << 0,3;
+        result.col(4) << -4.66666, -4;
+        result.col(5) << 2, -4;
+        result.col(6) << 2,-3;
+        result.col(7) << -3,-3;
+        return result;
+    }else if (which == 2) {
+        // Z structure
+        hector_math::Polygon<Scalar> result(2, 8);
+        result.col(0) << 0, 5;
+        result.col(1) << -3.6, 3.5;
+        result.col(2) << -5, 0;
+        result.col(3) << -3.6, -3.5;
+        result.col(4) << 0, -5;
+        result.col(5) << 3.6, -3.5;
+        result.col(6) << 5, 0;
+        result.col(7) << 3.6, 3.5;
+        return result;
+    }
+}
+template<typename Scalar>
+void verifyPositions(const std::vector<Vector2<Scalar>> &encounteredPositions, const std::vector<Vector2<Scalar>> &realPositions,std::string msg){
+    int a = (int) encounteredPositions.size();
+    //ASSERT_EQ(encounteredPositions.size(),realPositions.size())<<"Number of encountered positions and real positions doesn't match";
+    for (auto encounteredPosition:encounteredPositions){
+        bool exist = false;
+        for(auto real_position:realPositions){
+            if (encounteredPosition == real_position) exist=true;
+        }
+        EXPECT_TRUE(exist)<<encounteredPosition[0]<<", "<<encounteredPosition[1]<<" not in real Positions in "<<msg;
+    }
+    for(auto real_position:realPositions) {
+        bool exist = false;
+        for (auto encounteredPosition:encounteredPositions){
+            //if (pow(encounteredPosition[0]-real_position[0]+0.5,2),pow(encounteredPosition[1]-real_position[1]+0.5,2)<0.3) exist=true;
+            if (encounteredPosition == real_position) exist=true;
+        }
+        EXPECT_TRUE(exist)<<real_position[0]<<", "<<real_position[1]<<" not in encounterd Positions in "<<msg;
+    }
+    // verify that every entry is only once in encountered positions
+    for(auto i = encounteredPositions.begin(); i != encounteredPositions.end(); ++i){
+        int count = 0;
+        bool not_twice = true;
+        for(auto j = i + 1; j != encounteredPositions.end(); ++j){
+            if(*i == *j) not_twice= false;
+        }
+        EXPECT_TRUE(not_twice)<<*i<<"exists twice in the list of encountered Positions while iterating through the polygon/circle in "<<msg;
+    }
+}
+
+template<typename Scalar>
+class IteratorTest : public testing::Test
+{
+};
+
+
+typedef testing::Types<float, double> Implementations;
+
+TYPED_TEST_CASE( IteratorTest, Implementations );
+
+TYPED_TEST( IteratorTest, circleTest ) {
+    using Scalar = TypeParam;
+    using Vector2 = Vector2<Scalar>;
+
+    std::vector<Vector2> position;
+    //normal case in area x: -4 bis 4 and y: -4 bis 4, center (0,0) and radius 2,
+    iterateCircle<Scalar>(Vector2(Scalar(0.49),Scalar(0.49)),2,Eigen::Index(-4),Eigen::Index(4),Eigen::Index(-4),Eigen::Index(4) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    std::vector<Vector2> realPositions{Vector2(0,-2),Vector2(-1,-1),Vector2(0,-1),Vector2(1,-1),
+                                         Vector2(-2,0),Vector2(-1,0),Vector2(0,0),Vector2(1,0),
+                                         Vector2(-1,1),Vector2(0,1),Vector2(1,1)};
+    verifyPositions<Scalar>(position,realPositions,"normal case, center at (0.49,0.49");
+    // case 2, equal to one but center at (0,0)
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(-4),Eigen::Index(4),Eigen::Index(-4),Eigen::Index(4) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {Vector2(-1,-2),Vector2(0,-2),
+                     Vector2(-2,-1),Vector2(-1,-1),Vector2(0,-1),Vector2(1,-1),
+                     Vector2(-2,0),Vector2(-1,0),Vector2(0,0),Vector2(1,0),
+                     Vector2(-1,1),Vector2(0,1)};
+    verifyPositions<Scalar>(position,realPositions,"normal case center at (0,0)");
+    //case 3 restrict max_row to be 1
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(-4),Eigen::Index(1),Eigen::Index(-4),Eigen::Index(4) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {Vector2(-1,-2),Vector2(0,-2),
+                     Vector2(-2,-1),Vector2(-1,-1),Vector2(0,-1),
+                     Vector2(-2,0),Vector2(-1,0),Vector2(0,0),
+                     Vector2(-1,1),Vector2(0,1)};
+    verifyPositions<Scalar>(position,realPositions," case with center at (0,0) and limited max_row");
+
+    //case 4 restrict min_row to be 1
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(1),Eigen::Index(4),Eigen::Index(-4),Eigen::Index(4) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {Vector2(1,0),Vector2(1,-1)};
+    verifyPositions<Scalar>(position,realPositions," case with center at (0,0) and limited min_row");
+
+    //case 5 restrict min_column to be -1 and max_column to be 1
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(-4),Eigen::Index(4),Eigen::Index(-1),Eigen::Index(1) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {Vector2(-2,-1),Vector2(-1,-1),Vector2(0,-1),Vector2(1,-1),
+                     Vector2(-2,0),Vector2(-1,0),Vector2(0,0),Vector2(1,0)};
+    verifyPositions<Scalar>(position,realPositions," case with center at (0,0) and limited min/max_column");
+
+    //case 6 no points due to colum/index restrictions
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(4),Eigen::Index(8),Eigen::Index(-8),Eigen::Index(8) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(-8),Eigen::Index(8),Eigen::Index(4),Eigen::Index(8) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {};
+    verifyPositions<Scalar>(position,realPositions," case with center at (0,0) and all points should be invalid due to column/row restrictions");
+
+    // case 7, using different function overload
+    position.clear();
+    iterateCircle<Scalar>(Vector2(Scalar(0),Scalar(0)),2,Eigen::Index(4),Eigen::Index(4) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions = {Vector2(0,0),Vector2(1,0),Vector2(0,1)};
+    verifyPositions<Scalar>(position,realPositions," case with center at (0,0) usage of function overload setting min_ro/column to zeros");
+
+}
+
+TYPED_TEST( IteratorTest, polygonTest ) {
+    using Scalar = TypeParam;
+    using Vector2 = Vector2<Scalar>;
+    std::vector<Vector2> position;
+    Polygon<Scalar> polygon = createPolygon<Scalar>(0);
+    //normal case in area x: -4 bis 4 and y: -4 bis 4, center (0,0) and radius 2,
+    iteratePolygon<Scalar>(polygon,Eigen::Index(-6),Eigen::Index(6),Eigen::Index(-6),Eigen::Index(6) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    std::vector<Vector2> realPositions{{0,4},{1,4},{2,4},{3,4},
+                                       {-3,3},{-1,3},{0,3},{1,3},{2,3},{3,3},
+                                       {-4,2},{-3,2},{-2,2},{-1,2},{0,2},{1,2},{2,2},{3,2},
+                                       {-4,1},{-3,1},{-2,1},{-1,1},
+                                       {-4,0},{-3,0},{-2,0},{-1,0},{0,0},{1,0},
+                                       {-4,-1},{-3,-1},{-2,-1},{-1,-1},
+                                       {-2,-2}};//,{-1,-2}};
+    //verifyPositions<Scalar>(position,realPositions," case with 'random shape");
+    // case Z
+    polygon = createPolygon<Scalar>(1);
+    position.clear();
+    iteratePolygon<Scalar>(polygon,Eigen::Index(-6),Eigen::Index(6),Eigen::Index(-6),Eigen::Index(6) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions={{-5,3},{-4,3},{-3,3},{-2,3},{-1,3},{0,3},
+                  {0,2},
+                  {-1,1},
+                  {-2,0},
+                  {-2,-1},
+                  {-3,-2},{-2,-2},
+                   {-4,-3},
+                   {-5,-4},{-4,-4},{-3,-4},{-2,-4},{-1,-4},{0,-4}};
+    verifyPositions<Scalar>(position,realPositions," case with Z-shape");
+    // circle approximation
+    polygon = createPolygon<Scalar>(2);
+    position.clear();
+    iteratePolygon<Scalar>(polygon,Eigen::Index(-6),Eigen::Index(6),Eigen::Index(-6),Eigen::Index(6) ,[ &position ]( Eigen::Index x, Eigen::Index y )
+    {
+        position.push_back(Vector2(x,y));
+    } );
+    realPositions={{-1.0,-5.0},{0.0,-5.0},
+                   {-4.0,-4.0},{-3.0,-4.0},{-2.0,-4.0},{-1.0,-4.0},{0.0,-4.0},{1.0,-4.0},{2.0,-4.0},{3.0,-4.0},
+                   {-4.0,-3.0},{-3.0,-3.0},{-2.0,-3.0},{-1.0,-3.0},{0.0,-3.0},{1.0,-3.0},{2.0,-3.0},{3.0,-3.0},
+                   {-4.0,-2.0},{-3.0,-2.0},{-2.0,-2.0},{-1.0,-2.0},{0.0,-2.0},{1.0,-2.0},{2.0,-2.0},{3.0,-2.0},
+                   {-5.0,-1.0},{-4.0,-1.0},{-3.0,-1.0},{-2.0,-1.0},{-1.0,-1.0},{0.0,-1.0},{1.0,-1.0},{2.0,-1.0},{3.0,-1.0},{4.0,-1.0},
+                   {-5.0,0.0},{-4.0,0.0},{-3.0,0.0},{-2.0,0.0},{-1.0,0.0},{0.0,0.0},{1.0,0.0},{2.0,0.0},{3.0,0.0},{4.0,0.0},
+                   {-4.0,1.0},{-3.0,1.0},{-2.0,1.0},{-1.0,1.0},{0.0,1.0},{1.0,1.0},{2.0,1.0},{3.0,1.0},
+                   {-4.0,2.0},{-3.0,2.0},{-2.0,2.0},{-1.0,2.0},{0.0,2.0},{1.0,2.0},{2.0,2.0},{3.0,2.0},
+                   {-4.0,3.0},{-3.0,3.0},{-2.0,3.0},{-1.0,3.0},{0.0,3.0},{1.0,3.0},{2.0,3.0},{3.0,3.0},
+                   {-1.0,4.0},{0.0,4.0}};
+    verifyPositions<Scalar>(position,realPositions," case with circle shape");
+    // TODO: Test row and column min and max restrictions
+}
+
+int main( int argc, char **argv )
+{
+    ros::init( argc, argv, "test_graph_map" );
+    testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
+}
