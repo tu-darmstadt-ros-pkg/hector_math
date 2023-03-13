3130178279e31efc91b62be3cb2a29999c686619
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2023-03-13 12:05:53 +0100

    Fixed formatting.

--- hector_math_ros/include/hector_math_ros/message_conversions/gridmap_msgs.hpp
+++ hector_math_ros/include/hector_math_ros/message_conversions/gridmap_msgs.hpp
@@ -4,7 +4,8 @@
 #ifndef HECTOR_MATH_ROS_GRIDMAP_MSGS_HPP
 #define HECTOR_MATH_ROS_GRIDMAP_MSGS_HPP
 
-#warning "This header was moved for consistency. Please use the gridmap_msgs.h header. This header will be removed after 01.06.2023."
+#warning                                                                                           \
+    "This header was moved for consistency. Please use the gridmap_msgs.h header. This header will be removed after 01.06.2023."
 #include "gridmap_msgs.h"
 
 #endif // HECTOR_MATH_ROS_GRIDMAP_MSGS_HPP
--- hector_math_ros/include/hector_math_ros/urdf/conversions.hpp
+++ hector_math_ros/include/hector_math_ros/urdf/conversions.hpp
@@ -4,7 +4,8 @@
 #ifndef HECTOR_MATH_ROS_CONVERSIONS_HPP
 #define HECTOR_MATH_ROS_CONVERSIONS_HPP
 
-#warning "This header was moved for consistency. Please use the conversions.h header. This header will be removed after 01.06.2023."
+#warning                                                                                           \
+    "This header was moved for consistency. Please use the conversions.h header. This header will be removed after 01.06.2023."
 #include "conversions.h"
 
 #endif // HECTOR_MATH_ROS_CONVERSIONS_HPP
