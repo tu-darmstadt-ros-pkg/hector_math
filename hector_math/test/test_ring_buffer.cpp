934aa7078318b5b2d390b5ba4e4b942feaa35a31
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2023-03-23 17:24:39 +0100

    Simplified tests and removed test that tests index operator not being relative to head.

--- hector_math/test/test_ring_buffer.cpp
+++ hector_math/test/test_ring_buffer.cpp
@@ -3,27 +3,18 @@
 #include <gtest/gtest.h>
 
 using namespace hector_math;
-template<typename Scalar>
-class RingBufferTest : public testing::Test
+
+TEST( RingBufferTest, basic )
 {
-};
-
-typedef testing::Types<float, double> Implementations;
-
-TYPED_TEST_CASE( RingBufferTest, Implementations );
-
-TYPED_TEST( RingBufferTest, basic )
-{
-  using Scalar = TypeParam;
   constexpr size_t max_size = 50;
-  RingBuffer<Scalar, max_size> ringBuffer;
+  RingBuffer<int, max_size> ringBuffer;
   ASSERT_FALSE( ringBuffer.full() );
   ASSERT_TRUE( ringBuffer.empty() );
-  ringBuffer.push_back( Scalar( 0 ) );
+  ringBuffer.push_back( 0 );
   ASSERT_FALSE( ringBuffer.empty() );
   ASSERT_EQ( ringBuffer.size(), 1 );
   ASSERT_EQ( ringBuffer.capacity(), max_size );
-  for ( size_t i = 1; i < max_size - 1; i++ ) {
+  for ( int i = 1; i < max_size - 1; i++ ) {
     ringBuffer.push_back( i );
     ASSERT_EQ( i + 1, ringBuffer.size() );
     ASSERT_FALSE( ringBuffer.full() );
@@ -33,12 +24,24 @@ TYPED_TEST( RingBufferTest, basic )
   ASSERT_FALSE( ringBuffer.empty() );
   ASSERT_TRUE( ringBuffer.full() );
 
+  // begin() and [0] point to same element
+  ASSERT_EQ( *ringBuffer.begin(), ringBuffer[0] );
+
   ASSERT_EQ( ringBuffer.read_and_pop_front(), 0 );
   ASSERT_FALSE( ringBuffer.empty() );
   ASSERT_FALSE( ringBuffer.full() );
   ASSERT_EQ( 1, ringBuffer.read_and_pop_front() );
   ringBuffer.push_back( 50 );
   ASSERT_EQ( ringBuffer.size(), 49 );
+  ASSERT_EQ( *ringBuffer.begin(), 2 );
+
+  ringBuffer.push_back( 51 );
+  ASSERT_TRUE( ringBuffer.full() );
+  ASSERT_EQ( *ringBuffer.begin(), 2 );
+
+  // begin() and [0] point to same element
+  ASSERT_EQ( *ringBuffer.begin(), ringBuffer[0] );
+
   // CLEAR
   ringBuffer.clear();
   ASSERT_TRUE( ringBuffer.empty() );
@@ -46,11 +49,31 @@ TYPED_TEST( RingBufferTest, basic )
   ASSERT_EQ( ringBuffer.size(), 0 );
 }
 
-TYPED_TEST( RingBufferTest, iterators )
+TEST( RingBufferTest, arithmetic )
 {
-  using Scalar = TypeParam;
   constexpr size_t max_size = 50;
-  RingBuffer<Scalar, max_size> ringBuffer;
+  RingBuffer<int, max_size> ringBuffer;
+  for ( size_t i = 0; i < max_size; ++i ) ringBuffer.push_back( i );
+  ASSERT_TRUE( ringBuffer.full() );
+
+  auto it = ringBuffer.begin();
+  EXPECT_EQ( ringBuffer.end() - it, max_size );
+  auto it2 = it + 2;
+  EXPECT_EQ( it2 - it, 2 );
+  EXPECT_EQ( it - it2, -2 );
+
+  ringBuffer.push_back( 0 );
+  it = ringBuffer.begin();
+  EXPECT_EQ( ringBuffer.end() - it, max_size );
+  it2 = it + 2;
+  EXPECT_EQ( it2 - it, 2 );
+  EXPECT_EQ( it - it2, -2 );
+}
+
+TEST( RingBufferTest, iterators )
+{
+  constexpr size_t max_size = 50;
+  RingBuffer<int, max_size> ringBuffer;
   // FILL RING BUFFER PARTIAL
   for ( int i = 0; i < 25; i++ ) { ringBuffer.push_back( i ); }
   ASSERT_EQ( ringBuffer.size(), 25 );
@@ -67,11 +90,14 @@ TYPED_TEST( RingBufferTest, iterators )
   ASSERT_TRUE( ringBuffer.full() );
   index = 0;
   for ( auto elm : ringBuffer ) { ASSERT_EQ( index++, elm ); }
-  ASSERT_EQ( index, ringBuffer.size() );
+  ASSERT_EQ( index, ringBuffer.size() ) << "Did not iterate all elements!";
+
+  // begin() and [0] point to same element
+  ASSERT_EQ( *ringBuffer.begin(), ringBuffer[0] );
 
   // algorithms
   //  FOR_EACH
-  auto replace_with_3 = []( Scalar &n ) { n = 3; };
+  auto replace_with_3 = []( int &n ) { n = 3; };
   std::for_each( ringBuffer.begin(), ringBuffer.end(), replace_with_3 );
   for ( auto elm : ringBuffer ) { ASSERT_EQ( 3, elm ); }
   // FILL AGAIN AND OVERWRITE FIRST ELEMENTS
@@ -88,18 +114,16 @@ TYPED_TEST( RingBufferTest, iterators )
   ASSERT_EQ( ringBuffer.back(), max_size - 1 );
 }
 
-TYPED_TEST( RingBufferTest, iterators_random_access )
+TEST( RingBufferTest, iterators_random_access )
 {
-  using Scalar = TypeParam;
   constexpr size_t max_size = 50;
   constexpr size_t partial_filled = 25;
   for ( int start_pos = 0; start_pos < max_size; start_pos++ ) {
     // Instantiate RingBuffer and fill and remove items such that tail and head
     // start at start_pos -> test access for all possible variants
-    RingBuffer<Scalar, max_size> ringBuffer;
+    RingBuffer<int, max_size> ringBuffer;
     for ( int i = 0; i < start_pos; i++ ) { ringBuffer.push_back( -1 ); }
     for ( int i = 0; i < start_pos; i++ ) { ringBuffer.pop_front(); }
-    ASSERT_EQ( &ringBuffer.front(), &ringBuffer[start_pos] );
     // FILL RING BUFFER PARTIAL
     for ( int i = 0; i < partial_filled; i++ ) { ringBuffer.push_back( i ); }
 
@@ -165,22 +189,20 @@ TYPED_TEST( RingBufferTest, iterators_random_access )
   }
 }
 
-TYPED_TEST( RingBufferTest, front_back )
+TEST( RingBufferTest, front_back )
 {
-  using Scalar = TypeParam;
   constexpr size_t max_size = 50;
-  RingBuffer<Scalar, max_size> ringBuffer;
+  RingBuffer<int, max_size> ringBuffer;
   // FILL RING BUFFER PARTIAL
   for ( int i = 0; i < 25; i++ ) { ringBuffer.push_back( i ); }
   ASSERT_EQ( ringBuffer.front(), 0 );
   ASSERT_EQ( ringBuffer.back(), 24 );
 }
 
-TYPED_TEST( RingBufferTest, read_front )
+TEST( RingBufferTest, read_front )
 {
-  using Scalar = TypeParam;
   constexpr size_t max_size = 50;
-  RingBuffer<Scalar, max_size> ringBuffer;
+  RingBuffer<int, max_size> ringBuffer;
   // FILL RING BUFFER PARTIAL
   for ( int i = 0; i < 25; i++ ) { ringBuffer.push_back( i ); }
   // READ ELEMENTS
