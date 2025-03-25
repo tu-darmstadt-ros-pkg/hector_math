fc2c2883c04325fe24d49dc77b31a310e96006bf
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-03-25 15:29:59 +0100

    Added iterator classes. (#20)
    Adds iterator classes in addition to function for easy use in range-based for loops.
    
    (cherry picked from commit 2dc6e0080c48319e45a01bf1a00d81789a7e2ae1)

--- hector_math/include/hector_math/iterators/circle_iterator.h
+++ hector_math/include/hector_math/iterators/circle_iterator.h
@@ -44,6 +44,82 @@ void iterateCircle( const Vector2<T> &center, double radius, Functor functor )
   iterateCircle( center, radius, min, max, min, max, functor );
 }
 
+template<bool COLUMN_MAJOR = true>
+class CircleIndexIterator
+{
+public:
+  //! Overload iterating the given circle where the limits are taken from the given Eigen dense base (array/matrix).
+  //! @see CircleIndexIterator::CircleIndexIterator(const Vector2d &, double, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  template<typename Derived>
+  CircleIndexIterator( const Eigen::DenseBase<Derived> &map, const Vector2d &center, double radius )
+      : CircleIndexIterator( center, radius, 0, map.rows(), 0, map.cols() )
+  {
+  }
+
+  //! Overload where min x and y are 0 and max x and y are the given rows and cols.
+  //! @see CircleIndexIterator::CircleIndexIterator(const Vector2d &, double, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  CircleIndexIterator( const Vector2d &center, double radius, Eigen::Index rows, Eigen::Index cols )
+      : CircleIndexIterator( center, radius, 0, rows, 0, cols )
+  {
+  }
+
+  //! Overload where the indexes are not bounded.
+  //! @see CircleIndexIterator::CircleIndexIterator(const Vector2d &, double, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  CircleIndexIterator( const Vector2d &center, double radius )
+      : CircleIndexIterator( center, radius, std::numeric_limits<Eigen::Index>::lowest(),
+                             std::numeric_limits<Eigen::Index>::max(),
+                             std::numeric_limits<Eigen::Index>::lowest(),
+                             std::numeric_limits<Eigen::Index>::max() )
+  {
+  }
+
+  /*!
+   * Iterates over the given circle with the given center and radius.
+   * The iteration can be limited to a specific range within the circle using the x/y_min and x/y_max values.
+   * Note that the circle has to be in the index space, hence, if it is in map coordinates it might
+   * be necessary to convert it to the index space.
+   *
+   * The limits are given as [x_min, x_max) and [y_min, y_max) where x_min and y_min are included
+   * but x_max and y_max are excluded, i.e., the largest x index that will be iterated over is x_max - 1.
+   *
+   * @param center The center of the circle in the index space.
+   * @param radius The radius of the circle in the index space.
+   * @param x_min The minimum x index that will be iterated over.
+   * @param x_max The maximum x index that will be iterated over. This index is excluded.
+   * @param y_min The minimum y index that will be iterated over.
+   * @param y_max The maximum y index that will be iterated over. This index is excluded.
+   */
+  CircleIndexIterator( const Vector2d &center, double radius, Eigen::Index x_min,
+                       Eigen::Index x_max, Eigen::Index y_min, Eigen::Index y_max )
+      : center_( center ), radius_squared_( radius * radius )
+  {
+    if constexpr ( COLUMN_MAJOR ) {
+      x_min_ = x_min;
+      x_max_ = x_max;
+      y_min_ = std::max<Eigen::Index>( y_min, std::round( double( center.y() ) - radius ) );
+      y_max_ = std::min<Eigen::Index>( y_max, std::round( double( center.y() ) + radius ) );
+    } else {
+      y_min_ = y_min;
+      y_max_ = y_max;
+      x_min_ = std::max<Eigen::Index>( x_min, std::round( double( center.x() ) - radius ) );
+      x_max_ = std::min<Eigen::Index>( x_max, std::round( double( center.x() ) + radius ) );
+    }
+  }
+
+  struct iterator;
+
+  iterator begin();
+  iterator end();
+
+private:
+  const Vector2d center_;
+  double radius_squared_;
+  Eigen::Index x_min_;
+  Eigen::Index x_max_;
+  Eigen::Index y_min_;
+  Eigen::Index y_max_;
+};
+
 template<typename T, typename Functor>
 void iterateCircle( const Vector2<T> &center, double radius, Eigen::Index row_min,
                     Eigen::Index row_max, Eigen::Index col_min, Eigen::Index col_max, Functor functor )
@@ -63,6 +139,112 @@ void iterateCircle( const Vector2<T> &center, double radius, Eigen::Index row_mi
         std::min<Eigen::Index>( row_max, std::round( double( center.x() ) + width ) );
     for ( Eigen::Index x = min_x; x < max_x; ++x ) { functor( x, y ); }
   }
+}
+
+template<bool COLUMN_MAJOR>
+struct CircleIndexIterator<COLUMN_MAJOR>::iterator {
+private:
+  friend class CircleIndexIterator<COLUMN_MAJOR>;
+  const CircleIndexIterator<COLUMN_MAJOR> *circle_;
+  Eigen::Index max;
+  Index2D index;
+
+public:
+  iterator( const CircleIndexIterator<COLUMN_MAJOR> *circle, Eigen::Index x, Eigen::Index y,
+            Eigen::Index max )
+      : circle_( circle ), max( max ), index( x, y )
+  {
+  }
+
+  friend bool operator!=( const iterator &a, const iterator &b ) { return a.index != b.index; }
+
+  const Index2D &operator*() const { return index; }
+
+  iterator operator++()
+  {
+    if constexpr ( COLUMN_MAJOR ) {
+      if ( ++index.row < max ) {
+        return *this;
+      }
+      if ( ++index.col < circle_->y_max_ ) {
+        const double delta_y = double( index.col ) + 0.5 - double( circle_->center_.y() );
+        const double width = std::sqrt( circle_->radius_squared_ - delta_y * delta_y );
+        index.row = std::max<Eigen::Index>( circle_->x_min_,
+                                            std::round( double( circle_->center_.x() ) - width ) );
+        max = std::min<Eigen::Index>( circle_->x_max_,
+                                      std::round( double( circle_->center_.x() ) + width ) );
+        if ( index.row >= max ) {
+          ++( *this );
+        }
+        return *this;
+      }
+      index.col = circle_->y_max_;
+      index.row = 0;
+      max = 0;
+      return *this;
+    } else {
+      if ( ++index.col < max ) {
+        return *this;
+      }
+      if ( ++index.row < circle_->x_max_ ) {
+
+        const double delta_x = double( index.row ) + 0.5 - double( circle_->center_.x() );
+        const double height = std::sqrt( circle_->radius_squared_ - delta_x * delta_x );
+        index.col = std::max<Eigen::Index>( circle_->y_min_,
+                                            std::round( double( circle_->center_.y() ) - height ) );
+        max = std::min<Eigen::Index>( circle_->y_max_,
+                                      std::round( double( circle_->center_.y() ) + height ) );
+        if ( index.col >= max ) {
+          ++( *this );
+        }
+        return *this;
+      }
+      index.row = circle_->x_max_;
+      index.col = 0;
+      max = 0;
+      return *this;
+    }
+  }
+};
+
+template<bool COLUMN_MAJOR>
+typename CircleIndexIterator<COLUMN_MAJOR>::iterator CircleIndexIterator<COLUMN_MAJOR>::begin()
+{
+  if constexpr ( COLUMN_MAJOR ) {
+    // Formula of a circle is: r^2 = x^2 + y^2 ==> x = +/- sqrt(r^2 - y^2) which we can use to get min x and max x
+    const double delta_y = double( y_min_ ) + 0.5 - double( center_.y() );
+    const double width = std::sqrt( radius_squared_ - delta_y * delta_y );
+    const Eigen::Index start_x =
+        std::max<Eigen::Index>( x_min_, std::round( double( center_.x() ) - width ) );
+    const Eigen::Index end_x =
+        std::min<Eigen::Index>( x_max_, std::round( double( center_.x() ) + width ) );
+    auto it = iterator( this, start_x, y_min_, end_x );
+    if ( it.index.row >= it.max ) {
+      ++it;
+    }
+    return it;
+  } else {
+    const double delta_x = double( x_min_ ) + 0.5 - double( center_.x() );
+    const double height = std::sqrt( radius_squared_ - delta_x * delta_x );
+    const Eigen::Index start_y =
+        std::max<Eigen::Index>( y_min_, std::round( double( center_.y() ) - height ) );
+    const Eigen::Index end_y =
+        std::min<Eigen::Index>( y_max_, std::round( double( center_.y() ) + height ) );
+    auto it = iterator( this, x_min_, start_y, end_y );
+    if ( it.index.col >= it.max ) {
+      ++it;
+    }
+    return it;
+  }
+}
+
+template<bool COLUMN_MAJOR>
+typename CircleIndexIterator<COLUMN_MAJOR>::iterator CircleIndexIterator<COLUMN_MAJOR>::end()
+{
+  if constexpr ( COLUMN_MAJOR ) {
+    return iterator( this, 0, y_max_, 0 );
+  }
+  return iterator( this, x_max_, 0, 0 );
 }
 } // namespace hector_math
 
--- hector_math/include/hector_math/iterators/polygon_iterator.h
+++ hector_math/include/hector_math/iterators/polygon_iterator.h
@@ -45,6 +45,127 @@ void iteratePolygon( const Polygon<T> &polygon, Functor functor )
   iteratePolygon( polygon, min, max, min, max, functor );
 }
 
+template<bool COLUMN_MAJOR = true>
+class PolygonIndexIterator
+{
+public:
+  //! Overload iterating the given polygon where the limits are taken from the given Eigen dense base (array/matrix).
+  //! @see PolygonIndexIterator::PolygonIndexIterator(const Polygond &, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  template<typename Derived>
+  PolygonIndexIterator( const Eigen::DenseBase<Derived> &map, const Polygond &polygon )
+      : PolygonIndexIterator( polygon, 0, map.rows(), 0, map.cols() )
+  {
+  }
+
+  //! Overload where min x and y are 0 and max x and y are the given rows and cols.
+  //! @see PolygonIndexIterator::PolygonIndexIterator(const Polygond &, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  PolygonIndexIterator( const Polygond &polygon, Eigen::Index rows, Eigen::Index cols )
+      : PolygonIndexIterator( polygon, 0, rows, 0, cols )
+  {
+  }
+
+  //! Overload where the indexes are not bounded.
+  //! @see PolygonIndexIterator::PolygonIndexIterator(const Polygond &, Eigen::Index, Eigen::Index, Eigen::Index, Eigen::Index)
+  PolygonIndexIterator( const Polygond &polygon )
+      : PolygonIndexIterator( polygon, std::numeric_limits<Eigen::Index>::lowest(),
+                              std::numeric_limits<Eigen::Index>::max(),
+                              std::numeric_limits<Eigen::Index>::lowest(),
+                              std::numeric_limits<Eigen::Index>::max() )
+  {
+  }
+
+  /*!
+   * Iterates over the points in the given polygon.
+   * The iteration can be limited to a specific range within the polygon using the x/y_min and x/y_max values.
+   * Note that the polygon has to be in the index_ space, hence, if it is in map coordinates it might
+   * be necessary to convert it to the index_ space.
+   *
+   * The limits are given as [x_min, x_max) and [y_min, y_max) where x_min and y_min are included
+   * but x_max and y_max are excluded, i.e., the largest x index_ that will be iterated over is x_max - 1.
+   *
+   * @param polygon The polygon that is iterated over.
+   * @param x_min The minimum x index_ that will be iterated over.
+   * @param x_max The maximum x index_ that will be iterated over. This index_ is excluded.
+   * @param y_min The minimum y index_ that will be iterated over.
+   * @param y_max The maximum y index_ that will be iterated over. This index_ is excluded.
+   */
+  PolygonIndexIterator( const Polygond &polygon, Eigen::Index row_min, Eigen::Index row_max,
+                        Eigen::Index col_min, Eigen::Index col_max )
+      : polygon_( polygon ), row_min_( row_min ), row_max_( row_max ), col_min_( col_min ),
+        col_max_( col_max )
+  {
+  }
+
+  struct Line;
+  struct iterator;
+
+  iterator begin();
+  iterator end();
+
+private:
+  Polygond polygon_;
+  const Eigen::Index row_min_;
+  const Eigen::Index row_max_;
+  const Eigen::Index col_min_;
+  const Eigen::Index col_max_;
+};
+
+template<>
+struct PolygonIndexIterator<true>::Line {
+  Line() = default;
+
+  Line( const Pointd &a, const Pointd &b )
+  {
+    Pointd start = a;
+    Pointd end = b;
+    if ( a.y() > b.y() )
+      std::swap( start, end );
+    double diff_y = b.y() - a.y();
+    if ( std::abs( diff_y ) < 1E-4 )
+      x_increment = 0;
+    else
+      x_increment = double( b.x() - a.x() ) / double( b.y() - a.y() );
+
+    // Compute x value at center of y-column
+    x = start.x() + ( 0.5 - ( start.y() - std::floor( start.y() ) ) ) * x_increment;
+    start_y = start.y();
+    end_y = end.y();
+  }
+
+  double start_y;
+  double end_y;
+  double x;
+  double x_increment;
+};
+
+template<>
+struct PolygonIndexIterator<false>::Line {
+  Line() = default;
+
+  Line( const Pointd &a, const Pointd &b )
+  {
+    Pointd start = a;
+    Pointd end = b;
+    if ( a.x() > b.x() )
+      std::swap( start, end );
+    double diff_x = b.x() - a.x();
+    if ( std::abs( diff_x ) < 1E-4 )
+      y_increment = 0;
+    else
+      y_increment = double( b.y() - a.y() ) / double( b.x() - a.x() );
+
+    // Compute x value at center of y-column
+    y = start.y() + ( 0.5 - ( start.x() - std::floor( start.x() ) ) ) * y_increment;
+    start_x = start.x();
+    end_x = end.x();
+  }
+
+  double start_x;
+  double end_x;
+  double y;
+  double y_increment;
+};
+
 namespace detail
 {
 template<typename T, typename Functor, int LIMIT = 0>
@@ -74,50 +195,27 @@ void iteratePolygon( const Polygon<T> &polygon, Eigen::Index row_min, Eigen::Ind
                      Eigen::Index col_min, Eigen::Index col_max, Functor functor )
 {
   // Build iteration lines from the polygon points that allow us to get the x value for each
-  // discrete y index in the map
-  struct Line {
-    Line() = default;
-
-    Line( const Point<T> &a, const Point<T> &b )
-    {
-      Point<T> start = a;
-      Point<T> end = b;
-      if ( a.y() > b.y() )
-        std::swap( start, end );
-      T diff_y = b.y() - a.y();
-      if ( std::abs( diff_y ) < 1E-4 )
-        x_increment = 0;
-      else
-        x_increment = double( b.x() - a.x() ) / double( b.y() - a.y() );
-
-      // Compute x value at center of y-column
-      x = start.x() + ( 0.5 - ( start.y() - std::floor( start.y() ) ) ) * x_increment;
-      start_y = start.y();
-      end_y = end.y();
-    }
-
-    double start_y;
-    double end_y;
-    double x;
-    double x_increment;
-  };
+  // discrete y index_ in the map
+  using Line = PolygonIndexIterator<true>::Line;
   using LineContainer =
-      typename std::conditional<LIMIT == 0, std::vector<Line>, BoundedVector<Line, LIMIT + 1>>::type;
-  using RegionContainer = typename std::conditional<LIMIT == 0, std::vector<Eigen::Index>,
-                                                    BoundedVector<Eigen::Index, LIMIT>>::type;
+      std::conditional_t<LIMIT == 0, std::vector<Line>, BoundedVector<Line, LIMIT + 1>>;
+  using RegionContainer =
+      std::conditional_t<LIMIT == 0, std::vector<Eigen::Index>, BoundedVector<Eigen::Index, LIMIT>>;
 
   LineContainer lines;
   lines.reserve( polygon.cols() );
   Eigen::Index max_y = std::round( polygon.col( 0 ).y() );
   // Build lines from points and obtain max y for the stopping criterion during the iteration loop
   for ( Eigen::Index i = 0; i < polygon.cols() - 1; ++i ) {
-    lines.emplace_back( polygon.col( i ), polygon.col( i + 1 ) );
+    lines.emplace_back( polygon.col( i ).template cast<double>(),
+                        polygon.col( i + 1 ).template cast<double>() );
     Eigen::Index y_end = std::round( lines[i].end_y );
     if ( y_end > max_y )
       max_y = y_end;
   }
   max_y = std::min( max_y, col_max );
-  lines.emplace_back( polygon.col( polygon.cols() - 1 ), polygon.col( 0 ) );
+  lines.emplace_back( polygon.col( polygon.cols() - 1 ).template cast<double>(),
+                      polygon.col( 0 ).template cast<double>() );
 
   // Sort lines by their y start, to quickly find new active lines as we iterate over y
   std::sort( lines.begin(), lines.end(),
@@ -164,6 +262,211 @@ void iteratePolygon( const Polygon<T> &polygon, Eigen::Index row_min, Eigen::Ind
   }
 }
 } // namespace detail
+
+template<bool COLUMN_MAJOR>
+struct PolygonIndexIterator<COLUMN_MAJOR>::iterator {
+private:
+  std::vector<Line> lines_;
+  std::vector<Line> active_lines_;
+  std::vector<Eigen::Index> scan_line_region_segments_;
+  Index2D index_;
+  std::size_t active_line_index_ = 0;
+  std::size_t segment_index_ = 0;
+  Eigen::Index next_segment_switch_;
+  const Eigen::Index row_min_;
+  Eigen::Index row_max_;
+  const Eigen::Index col_min_;
+  Eigen::Index col_max_;
+  bool end_ = false;
+
+public:
+  iterator( const Polygond &polygon, Eigen::Index row_min, Eigen::Index row_max,
+            Eigen::Index col_min, Eigen::Index col_max )
+      : row_min_( row_min ), row_max_( row_max ), col_min_( col_min ), col_max_( col_max )
+  {
+    lines_.reserve( polygon.cols() );
+    // Build lines from points and obtain max y for the stopping criterion during the iteration loop
+    for ( Eigen::Index i = 0; i < polygon.cols() - 1; ++i ) {
+      lines_.emplace_back( polygon.col( i ), polygon.col( i + 1 ) );
+    }
+    lines_.emplace_back( polygon.col( polygon.cols() - 1 ), polygon.col( 0 ) );
+
+    if constexpr ( COLUMN_MAJOR ) {
+      Eigen::Index max_y = col_min;
+      for ( Eigen::Index i = 0; i < polygon.cols(); ++i ) {
+        max_y = std::max<Eigen::Index>( max_y, std::round( polygon.col( i ).y() ) );
+      }
+      col_max_ = std::min( col_max, max_y );
+      // Sort lines by their y start, to quickly find new active lines as we iterate over y
+      std::sort( lines_.begin(), lines_.end(),
+                 []( const Line &a, const Line &b ) { return a.start_y < b.start_y; } );
+      index_.col = std::max<Eigen::Index>( col_min, std::round( lines_[0].start_y ) );
+    } else {
+      Eigen::Index max_x = row_min;
+      for ( Eigen::Index i = 0; i < polygon.cols(); ++i ) {
+        max_x = std::max<Eigen::Index>( max_x, std::round( polygon.col( i ).x() ) );
+      }
+      row_max_ = std::min( row_max, max_x );
+      // Sort lines by their x start, to quickly find new active lines as we iterate over x
+      std::sort( lines_.begin(), lines_.end(),
+                 []( const Line &a, const Line &b ) { return a.start_x < b.start_x; } );
+      index_.row = std::max<Eigen::Index>( row_min, std::round( lines_[0].start_x ) );
+    }
+    updateSegments();
+    if constexpr ( COLUMN_MAJOR ) {
+      index_.row = std::max( row_min, scan_line_region_segments_[0] );
+      next_segment_switch_ = std::min( row_max, scan_line_region_segments_[1] );
+      if ( index_.row >= next_segment_switch_ )
+        ++( *this );
+    } else {
+      index_.col = std::max( col_min, scan_line_region_segments_[0] );
+      next_segment_switch_ = std::min( col_max, scan_line_region_segments_[1] );
+      if ( index_.col >= next_segment_switch_ )
+        ++( *this );
+    }
+  }
+
+  iterator()
+      : index_( 0, 0 ), next_segment_switch_( 0 ), row_min_( 0 ), row_max_( 0 ), col_min_( 0 ),
+        col_max_( 0 ), end_( true )
+  {
+  }
+
+  const Index2D &operator*() const { return index_; }
+
+  friend bool operator==( const iterator &a, const iterator &b )
+  {
+    return a.end_ == b.end_ || ( !a.end_ && !b.end_ && a.index_ == b.index_ );
+  }
+
+  friend bool operator!=( const iterator &a, const iterator &b ) { return !( a == b ); }
+
+  iterator &operator++()
+  {
+    if constexpr ( COLUMN_MAJOR ) {
+      if ( ++index_.row < next_segment_switch_ ) {
+        return *this;
+      }
+      segment_index_ += 2;
+      if ( segment_index_ >= scan_line_region_segments_.size() - 1 ) {
+        ++index_.col;
+        if ( index_.col >= col_max_ ) {
+          end_ = true;
+          index_.col = col_max_;
+          index_.row = 0;
+          next_segment_switch_ = 0;
+          scan_line_region_segments_.clear();
+          return *this;
+        }
+        updateSegments();
+        assert( scan_line_region_segments_.size() >= 2 );
+      }
+      index_.row = std::max( row_min_, scan_line_region_segments_[segment_index_] );
+      next_segment_switch_ = std::min( row_max_, scan_line_region_segments_[segment_index_ + 1] );
+      if ( index_.row >= next_segment_switch_ )
+        ++( *this );
+      return *this;
+    } else {
+      if ( ++index_.col < next_segment_switch_ ) {
+        return *this;
+      }
+      segment_index_ += 2;
+      if ( segment_index_ >= scan_line_region_segments_.size() - 1 ) {
+        ++index_.row;
+        if ( index_.row >= row_max_ ) {
+          end_ = true;
+          index_.row = row_max_;
+          index_.col = 0;
+          next_segment_switch_ = 0;
+          scan_line_region_segments_.clear();
+          return *this;
+        }
+        updateSegments();
+        assert( scan_line_region_segments_.size() >= 2 );
+      }
+      index_.col = std::max( col_min_, scan_line_region_segments_[segment_index_] );
+      next_segment_switch_ = std::min( col_max_, scan_line_region_segments_[segment_index_ + 1] );
+      if ( index_.col >= next_segment_switch_ )
+        ++( *this );
+      return *this;
+    }
+  }
+
+private:
+  void updateSegments()
+  {
+    if constexpr ( COLUMN_MAJOR ) {
+      const double y_cell_limit = double( index_.col ) + 0.5;
+      // Determine lines that ended
+      for ( int i = active_lines_.size() - 1; i >= 0; --i ) {
+        active_lines_[i].x += active_lines_[i].x_increment;
+        if ( active_lines_[i].end_y >= y_cell_limit )
+          continue;
+        active_lines_.erase( active_lines_.begin() + i );
+      }
+      // Determine new lines that started
+      for ( ; active_line_index_ < lines_.size(); ++active_line_index_ ) {
+        if ( lines_[active_line_index_].start_y >= y_cell_limit )
+          break;
+        if ( lines_[active_line_index_].end_y < y_cell_limit )
+          continue; // Ignore lines that start and end before current column
+        if ( lines_[active_line_index_].start_y < index_.col )
+          lines_[active_line_index_].x += lines_[active_line_index_].x_increment;
+        active_lines_.push_back( lines_[active_line_index_] );
+      }
+
+      // We obtain from each line the x for the current y and use that information to iterate between each pair of x(k) -> x(k+1) where k = 2 * i and i is a natural integer
+      scan_line_region_segments_.clear();
+      for ( std::size_t i = 0; i < active_lines_.size(); ++i ) {
+        Eigen::Index x = std::round( active_lines_[i].x );
+        scan_line_region_segments_.push_back( x );
+      }
+      std::sort( scan_line_region_segments_.begin(), scan_line_region_segments_.end() );
+      segment_index_ = 0;
+    } else {
+      const double x_cell_limit = double( index_.row ) + 0.5;
+      // Determine lines that ended
+      for ( int i = active_lines_.size() - 1; i >= 0; --i ) {
+        active_lines_[i].y += active_lines_[i].y_increment;
+        if ( active_lines_[i].end_x >= x_cell_limit )
+          continue;
+        active_lines_.erase( active_lines_.begin() + i );
+      }
+      // Determine new lines that started
+      for ( ; active_line_index_ < lines_.size(); ++active_line_index_ ) {
+        if ( lines_[active_line_index_].start_x >= x_cell_limit )
+          break;
+        if ( lines_[active_line_index_].end_x < x_cell_limit )
+          continue; // Ignore lines that start and end before current column
+        if ( lines_[active_line_index_].start_x < index_.row )
+          lines_[active_line_index_].y += lines_[active_line_index_].y_increment;
+        active_lines_.push_back( lines_[active_line_index_] );
+      }
+
+      // We obtain from each line the y for the current x and use that information to iterate between each pair of x(k) -> x(k+1) where k = 2 * i and i is a natural integer
+      scan_line_region_segments_.clear();
+      for ( std::size_t i = 0; i < active_lines_.size(); ++i ) {
+        Eigen::Index y = std::round( active_lines_[i].y );
+        scan_line_region_segments_.push_back( y );
+      }
+      std::sort( scan_line_region_segments_.begin(), scan_line_region_segments_.end() );
+      segment_index_ = 0;
+    }
+  }
+};
+
+template<bool COLUMN_MAJOR>
+typename PolygonIndexIterator<COLUMN_MAJOR>::iterator PolygonIndexIterator<COLUMN_MAJOR>::begin()
+{
+  return iterator( polygon_, row_min_, row_max_, col_min_, col_max_ );
+}
+
+template<bool COLUMN_MAJOR>
+typename PolygonIndexIterator<COLUMN_MAJOR>::iterator PolygonIndexIterator<COLUMN_MAJOR>::end()
+{
+  return iterator();
+}
+
 } // namespace hector_math
 
 #endif // HECTOR_MATH_POLYGON_ITERATOR_H
