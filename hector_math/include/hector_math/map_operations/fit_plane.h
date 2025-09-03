327c7ab292baeee150ed819251153b1e2dcc71be
Stefan Fabian <fabian@sim.tu-darmstadt.de>
2025-09-03 11:06:53 +0200

    Added robust fitPlane method.

--- hector_math/include/hector_math/map_operations/fit_plane.h
+++ hector_math/include/hector_math/map_operations/fit_plane.h
@@ -21,7 +21,7 @@ struct PlaneEstimationResult {
 };
 
 /*!
- * @brief Fits a plane to the given map.
+ * @brief Fits a plane to the given map using a least-squares fit.
  * @param map The 2D array of height values this plane is fitted to.
  * @param resolution The resolution of the map. Used to scale the gradient.
  */
@@ -36,19 +36,17 @@ PlaneEstimationResult fitPlaneXY( const Eigen::DenseBase<Derived> &map, const do
   Scalar col_sum = 0;
   long count = 0;
   Vector3d z = Vector3d::Zero();
-  for ( Eigen::Index col = 0; col < map.cols(); ++col ) {
-    for ( Eigen::Index row = 0; row < map.rows(); ++row ) {
-      const auto &value = map( row, col );
-      if ( !is_valid_fn( value ) )
-        continue;
-      row_squared_sum += row * row;
-      row_sum += row;
-      col_squared_sum += col * col;
-      col_sum += col;
-      row_col_sum += row * col;
-      ++count;
-      z += Vector3d( row * value, col * value, value );
-    }
+  for ( const auto &[row, col] : EigenIndexIterator( map ) ) {
+    const auto &value = map( row, col );
+    if ( !is_valid_fn( value ) )
+      continue;
+    row_squared_sum += row * row;
+    row_sum += row;
+    col_squared_sum += col * col;
+    col_sum += col;
+    row_col_sum += row * col;
+    ++count;
+    z += Vector3d( row * value, col * value, value );
   }
   Eigen::Matrix3d X;
   // clang-format off
@@ -57,7 +55,83 @@ PlaneEstimationResult fitPlaneXY( const Eigen::DenseBase<Derived> &map, const do
        row_sum,         col_sum,         count;
   // clang-format on
   Vector3d abc = X.inverse() * z;
-  PlaneEstimationResult result;
+  PlaneEstimationResult result = {};
+  result.gradient_x = static_cast<float>( abc( 0 ) / resolution );
+  result.gradient_y = static_cast<float>( abc( 1 ) / resolution );
+  result.center_plane_z = static_cast<float>( abc( 2 ) ) +
+                          result.gradient_x * ( map.rows() - 1 ) * resolution / 2 +
+                          result.gradient_y * ( map.cols() - 1 ) * resolution / 2;
+  result.percentage_known = static_cast<float>( count ) / ( map.rows() * map.cols() );
+  return result;
+}
+
+/*!
+ * @brief Fits a plane to the given map using only the X% best values
+ * @param map The 2D array of height values this plane is fitted to.
+ * @param resolution The resolution of the map. Used to scale the gradient.
+ */
+template<typename Derived, bool ( *is_valid_fn )( typename Eigen::DenseBase<Derived>::Scalar ) = std::isfinite>
+PlaneEstimationResult fitPlaneXYRobust( const Eigen::DenseBase<Derived> &map,
+                                        const double resolution = 1.0, const double min_percent = 0.6 )
+{
+  using Scalar = typename Eigen::DenseBase<Derived>::Scalar;
+  long count = 0;
+  Scalar min = std::numeric_limits<Scalar>::max();
+  Scalar max = std::numeric_limits<Scalar>::lowest();
+  Scalar sum = 0;
+  for ( const auto &val : EigenValueIterator( map ) ) {
+    if ( !is_valid_fn( val ) )
+      continue;
+    min = std::min( min, val );
+    max = std::max( max, val );
+    sum += val;
+    ++count;
+  }
+  const float mean = count > 0 ? sum / count : 0;
+  const float threshold = ( max - min ) * min_percent / 2;
+  count = 0;
+  for ( const auto &val : EigenValueIterator( map ) ) {
+    if ( !is_valid_fn( val ) )
+      continue;
+    if ( std::abs( val - mean ) > threshold )
+      continue; // Skip outliers
+    ++count;
+  }
+
+  if ( count < 0.5 * map.rows() * map.cols() ) {
+    // Not enough valid points to fit a plane like that. Fallback to non-robust fitting using all points
+    auto result = fitPlaneXY<Derived, is_valid_fn>( map, resolution );
+    result.center_plane_z = ( min + max ) / 2; // Use the average of min and max as center plane z
+    return result;
+  }
+
+  Scalar row_squared_sum = 0;
+  Scalar row_sum = 0;
+  Scalar col_squared_sum = 0;
+  Scalar row_col_sum = 0;
+  Scalar col_sum = 0;
+  count = 0;
+  Vector3d z = Vector3d::Zero();
+  for ( const auto &[row, col] : EigenIndexIterator( map ) ) {
+    const auto &value = map( row, col );
+    if ( !is_valid_fn( value ) || std::abs( value - mean ) > threshold )
+      continue;
+    row_squared_sum += row * row;
+    row_sum += row;
+    col_squared_sum += col * col;
+    col_sum += col;
+    row_col_sum += row * col;
+    ++count;
+    z += Vector3d( row * value, col * value, value );
+  }
+  Eigen::Matrix3d X;
+  // clang-format off
+  X << row_squared_sum, row_col_sum,     row_sum,
+       row_col_sum,     col_squared_sum, col_sum,
+       row_sum,         col_sum,         count;
+  // clang-format on
+  Vector3d abc = X.inverse() * z;
+  PlaneEstimationResult result = {};
   result.gradient_x = static_cast<float>( abc( 0 ) / resolution );
   result.gradient_y = static_cast<float>( abc( 1 ) / resolution );
   result.center_plane_z = static_cast<float>( abc( 2 ) ) +
